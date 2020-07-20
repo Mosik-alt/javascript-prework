83a634d7e1e443c718d13c88d1c1db14dc187f59
Magda Osik <magdalenaosik@gmail.com>
2020-07-20 19:57:08 +0200

    Add new files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let computerMove = `kamień`;
+('Zagrałem' + 'kamień' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier' ;
+('Zagrałeś' + 'papier' + '! Gratulacje wygrałeś!');
