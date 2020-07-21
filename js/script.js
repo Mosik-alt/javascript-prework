91f3c499aa53f81a8fda7244e70d015b8ae3e94e
Magda Osik <magdalenaosik@gmail.com>
2020-07-21 10:01:23 +0200

    Add new files

--- js/script.js
+++ js/script.js
@@ -13,5 +13,22 @@ else if (randomNumber  == 2) {
 else if (randomNumber  == 3) {
     computerMove = 'nożyce';
 }
+printMessage ('Mój ruch to: ' + computerMove); 
 
-printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+else if (playerInput =='2'){
+  playerMove ='papier' ;
+  }
+else if (playerInput =='3') {
+  playerMove ='nożyce' ;
+}
+printMessage('Twój ruch to: ' + playerMove);
