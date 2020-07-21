3e1af1d6d1632145aa9efab0497f8e8d1a2c195e
Magda Osik <magdalenaosik@gmail.com>
2020-07-21 21:50:35 +0200

    Add new files

--- js/script.js
+++ js/script.js
@@ -32,5 +32,33 @@ else if (playerInput =='3') {
 }
 printMessage('Twój ruch to: ' + playerMove);
 
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove =='kamień' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+else if (computerMove =='kamień' && playerMove == 'kamień'){
+  printMessage('Remis! Zagrajmy jeszcze raz!');
+}
+if( computerMove == 'papier' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove =='papier' && playerMove == 'nożyce'){
+  printMessage('Ty przegrywasz!');
+}
+else if (computerMove =='papier' && playerMove == 'papier'){
+  printMessage('Remis! Zagrajmy jeszcze raz!');
+}
+if( computerMove == 'nożyce' && playerMove == 'kamień'){
+  printMessage('Ty wygrywasz!');
+}
+else if (computerMove =='nożyce' && playerMove == 'papier'){
+  printMessage('Ty przegrywasz!');
+}
+else if (computerMove =='nożyce' && playerMove == 'nożyce'){
+  printMessage('Remis! Zagrajmy jeszcze raz!');
+}
+
 
 
